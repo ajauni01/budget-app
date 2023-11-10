4a99938f92d5500257d99902cde420c1021582fc
AJHARUL ISLAM AUNIK <aunik@AJHARULs-Air.ad.wsc.edu>
2023-11-10 12:33:59 -0600

    Add total price feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useRef, useState } from "react";
 
 function App() {
   // useState to hold the list of items
@@ -20,42 +20,41 @@ function App() {
       // add the item to the list of items
       setItems([...items, userInputValueItem]);
       // clear the input field
-      inputRefItem.current!.value = '';
+      inputRefItem.current!.value = "";
     }
     // get the value of the input field for adding of prices using the useRef hook
     const userInputValuePrice = inputRefPrice.current?.value;
-    if(userInputValuePrice){
+    if (userInputValuePrice) {
       // add the price to the list of prices
       setPrices([...prices, parseInt(userInputValuePrice)]);
       // clear the input field
-      inputRefPrice.current!.value = '';
+      inputRefPrice.current!.value = "";
     }
   };
 
   // handleDeleteItem function to handle the delete button click
-const handleDeleteItem = (index: number) => {
-  // create a copy of the items array
-  const newItems = [...items];
-  // remove the item at the specified index
-  newItems.splice(index, 1);
-  // update the state with the new items array
-  setItems(newItems);
+  const handleDeleteItem = (index: number) => {
+    // create a copy of the items array
+    const newItems = [...items];
+    // remove the item at the specified index
+    newItems.splice(index, 1);
+    // update the state with the new items array
+    setItems(newItems);
 
-  // create a copy of the prices array
-  const newPrices = [...prices];
-  // remove the price at the specified index
-  newPrices.splice(index, 1);
-  // update the state with the new prices array
-  setPrices(newPrices);
-};
+    // create a copy of the prices array
+    const newPrices = [...prices];
+    // remove the price at the specified index
+    newPrices.splice(index, 1);
+    // update the state with the new prices array
+    setPrices(newPrices);
+  };
 
   return (
-    <div className="text-white mt-20">
-      <h2 className="text-5xl mb-10">Budget App Version 2.0</h2>
+    <div className="mt-20 text-white">
+      <h2 className=" text-5xl mb-10">Budget App Version 2.0</h2>
       <form onSubmit={handleAddedItem} className="w-full max-w-sm">
-        
         <div className="flex mb-5">
-        <p className="text-2xl mr-12 mt-10">Name:</p>
+          <p className="text-2xl mr-12 mt-10">Name:</p>
           {/* input field to add items */}
           <input
             ref={inputRefItem}
@@ -66,12 +65,12 @@ const handleDeleteItem = (index: number) => {
             aria-label="Item Names"
           />
         </div>
-          {/* input field to add prices */}
-          <div className="flex mb-10">
-        <p className="text-2xl mr-6 mt-8">Amount:</p>
+        {/* input field to add prices */}
+        <div className="flex mb-10">
+          <p className="text-2xl mr-6 mt-8">Amount:</p>
           {/* input field to add items */}
           <input
-          required
+            required
             ref={inputRefPrice}
             className=" appearance-none bg-transparent border-b  border-teal-500 w-full mr-3 py-5 px-2 mb-5 leading-tight"
             type="number"
@@ -79,47 +78,49 @@ const handleDeleteItem = (index: number) => {
             aria-label="Item Prices"
           />
         </div>
-          
 
-          <button
-            className="w-full h-11 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
-            type="submit"
-          >
-            Add Items
-          </button>
-        
-      </form>
-      
-      {items.length > 0 ? (
-        <div className="flex">
-           {/* show the added items depending on user input */}
-  <div>
-    {items.map((item, index) => (
-      <div key={index} className="flex items-center mt-10">
         <button
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => handleDeleteItem(index)}
+          className="w-full h-11 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+          type="submit"
         >
-          X
+          Add Items
         </button>
-        <h3 className="text-2xl ml-4">{item}</h3>
-      </div>
-    ))}
-  </div>
+      </form>
 
-{/* show the added prices depending on the user input */}
-  <div>
- {prices.map((price, index) => (
-      <div key={index} className="flex items-center mt-10">
-        <h3 className="text-2xl ml-4 mt-1"> -${price}</h3>
-      </div>
-    ))}
-  </div>
+      {items.length > 0 ? (
+        <div>
+          <div className="flex">
+            {/* show the added items depending on user input */}
+            <div>
+              {items.map((item, index) => (
+                <div key={index} className="flex items-center mt-10">
+                  <button
+                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={() => handleDeleteItem(index)}
+                  >
+                    X
+                  </button>
+                  <h3 className="text-2xl ml-4">{item}</h3>
+                </div>
+              ))}
+            </div>
 
-  </div>
-) : (
-  <p className="text-5xl text-red-700 mt-10">No Budget Items</p>
-)}
+            {/* show the added prices depending on the user input */}
+            <div>
+              {prices.map((price, index) => (
+                <div key={index} className="flex items-center mt-10">
+                  <h3 className="text-2xl ml-4 mt-1"> -${price}</h3>
+                </div>
+              ))}
+            </div>
+          </div>
+          {/* show total price for the added items */}
+          <div className="underline mt-5 mb-5"></div>
+          <h3 className="text-2xl">Total: ${prices.reduce((a, b) => a + b)}</h3>
+        </div>
+      ) : (
+        <p className="text-5xl text-red-700 mt-10">No Budget Items</p>
+      )}
     </div>
   );
 }
